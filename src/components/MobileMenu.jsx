3c886f4c53b1c7b9d7d90580683de88f24d3c579
Siyabonga <siyabongagift658@gmail.com>
2026-04-19 20:18:50 +0200

    fly

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -20,37 +20,42 @@ function MobileMenu({ isOpen, toggleMenu }) {
 
     return (
         <div ref={menuRef}
-            className='fixed bottom-0 w-full max-h-[40svh] overflow-y-auto z-10 px-10 py-2 
-             bg-white/30 backdrop-blur-[50px] border-t border-white/20 
-             Menu glass-scroll rounded-3xl'>
+            className='fixed bottom-0 w-full h-[40svh] z-10 px-10 py-2 
+             bg-white/30 backdrop-blur-[50px] border-t rounded-b-0 border-white/20 
+             Menu glass-scroll rounded-t-3xl'>
             <div className='sticky top-2 dragger w-40 h-2 mx-auto rounded-md bg-white mb-15'></div>
-            <h3 className='text-[clamp(1rem,2.5vw,1rem)] font-semibold text-white/70 my-3'>OVERVIEW</h3>
-            <ul>
-                <li className='flex items-center text-white gap-3 p-3 font-light'>
-                    <i class="fa-solid fa-house"></i>Dashboard
-                </li>
-            </ul>
-            <h3 className='text-[clamp(1rem,2.5vw,1rem)] font-semibold text-white/70 my-3'>MANAGER</h3>
-            <ul>
-                <li className='flex items-center text-white gap-3 p-3 border-b font-light'>
-                    <i class="fa-solid fa-layer-group"></i>Scheme & Members
-                </li>
-                <li className='flex items-center text-white gap-3 p-3 border-b font-light'>
-                    <i class="fa-solid fa-file-invoice"></i>Expenses
-                </li>
-                <li className='flex items-center text-white gap-3 p-3 font-light'>
-                    <i class="fa-solid fa-chart-line"></i>Insights
-                </li>
-            </ul>
-            <h3 className='text-[clamp(1rem,2.5vw,1rem)] font-semibold text-white/70 my-3'>SYSTEM</h3>
-            <ul>
-                <li className='flex items-center text-white gap-3 p-3 font-light'>
-                    <i class="fa-solid fa-clock-rotate-left"></i>Activity History
-                </li>
-                <li className='flex items-center text-white gap-3 p-3 font-light'>
-                    <i class="fa-solid fa-gear"></i>Settings
-                </li>
-            </ul>
+
+            <div className='w-full max-h-70 overflow-y-auto glass-scroll pt-6'>
+                <h3 className='text-[clamp(1rem,2.5vw,1rem)] font-semibold text-white/70 my-3'>OVERVIEW</h3>
+                <ul>
+                    <li className='flex items-center text-white gap-3 p-3 font-light'>
+                        <i class="fa-solid fa-house"></i>Dashboard
+                    </li>
+                </ul>
+                <h3 className='text-[clamp(1rem,2.5vw,1rem)] font-semibold text-white/70 my-3'>MANAGER</h3>
+                <ul>
+                    <li className='flex items-center text-white gap-3 p-3 border-b font-light'>
+                        <i class="fa-solid fa-layer-group"></i>Scheme & Members
+                    </li>
+                    <li className='flex items-center text-white gap-3 p-3 border-b font-light'>
+                        <i class="fa-solid fa-file-invoice"></i>Expenses
+                    </li>
+                    <li className='flex items-center text-white gap-3 p-3 font-light'>
+                        <i class="fa-solid fa-chart-line"></i>Insights
+                    </li>
+                </ul>
+                <h3 className='text-[clamp(1rem,2.5vw,1rem)] font-semibold text-white/70 my-3'>SYSTEM</h3>
+                <ul>
+                    <li className='flex items-center text-white gap-3 p-3 font-light'>
+                        <i class="fa-solid fa-clock-rotate-left"></i>Activity History
+                    </li>
+                    <li className='flex items-center text-white gap-3 p-3 font-light'>
+                        <i class="fa-solid fa-gear"></i>Settings
+                    </li>
+                </ul>
+            </div>
+
+
         </div>
     )
 }
