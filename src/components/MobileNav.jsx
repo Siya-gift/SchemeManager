3d0f70b7c14202ddea1c8ead27abf34eea84228b
Siyabonga <siyabongagift658@gmail.com>
2026-04-19 13:48:56 +0200

    Nav Normal

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -10,25 +10,21 @@ function MobileNav() {
 
     return (
         <>
-            <div className='sticky top-2 z-1'>
-                <div className='profile bg-white/55 glass flex md:hidden justify-between items-center m-5
+            <div className='profile glass flex md:hidden justify-between items-center m-5
                  p-3 cursor-pointer hover:bg-white/45'>
-                    <div className='pp'>
-                        <div className="ppic static -top-1 left-[50%] w-10 h-10 rounded-full overflow-hidden">
-                            <img src={luffyImg} className="h-full w-full object-cover" />
-                        </div>
-                        <h1 className='text-white text-3xl font-bold'>Vusi Nkosi</h1>
-                        <small className='text-[#abe9be]'>Scheme Owner</small>
+                <div className='pp'>
+                    <div className="ppic static -top-1 left-[50%] w-10 h-10 rounded-full overflow-hidden">
+                        <img src={luffyImg} className="h-full w-full object-cover" />
                     </div>
-                    <div className='burger w-6.25' onClick={openMenu}>
-                        <div className='line1 h-0.75 w-full bg-white rounded-sm'></div>
-                        <div className='line2 h-0.75 w-full bg-white rounded-sm mt-1.25'></div>
-                        <div className='line3 h-0.75 w-full bg-white rounded-sm mt-1.25'></div>
-                    </div>
+                    <h1 className='text-white text-3xl font-bold'>Vusi Nkosi</h1>
+                    <small className='text-[#abe9be]'>Scheme Owner</small>
+                </div>
+                <div className='burger w-6.25' onClick={openMenu}>
+                    <div className='line1 h-0.75 w-full bg-white rounded-sm'></div>
+                    <div className='line2 h-0.75 w-full bg-white rounded-sm mt-1.25'></div>
+                    <div className='line3 h-0.75 w-full bg-white rounded-sm mt-1.25'></div>
                 </div>
             </div>
-
-            
         </>
 
 
