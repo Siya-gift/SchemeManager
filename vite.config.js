b51fa07805a3bf26fecb0fb17cbb79b5b95a670f
Siyabonga <siyabongagift658@gmail.com>
2026-09-10 15:40:38 +0200

    more updates

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,20 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
- plugins: [
+  plugins: [
     tailwindcss(),
     react()
   ],
+  server: {
+    watch: {
+      ignored: [
+        '**/node_modules/**',
+        '**/.git/**',
+        '**/vendor/**',     // Ignore PHP composer vendor folders
+        '**/venv/**',       // Ignore Python virtual environments
+        '**/storage/**',    // Ignore large backend storage/cache folders
+      ]
+    }
+  }
 })
 
